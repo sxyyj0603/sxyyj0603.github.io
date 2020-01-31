b5fe6ff85d56a9ef8e9ef374baa38283be7a8a5e
Lihua Lei <lihualei@Lihuas-MacBook-Pro.local>
2020-01-30 22:27:07 -0800

    new update Thu Jan 30 22:27:07 PST 2020

--- master.sh
+++ master.sh
@@ -6,13 +6,13 @@
 #./jemdoc.py coursework.jemdoc
 # python2.7jemdoc.py activities.jemdoc
 
-tac index.html | awk 'NR > 2' | tac > tmp.html
-cat tmp.html > index.html
-rm tmp.html
-cat counter.txt >> index.html
-echo "" >> index.html
-echo "</body>" >> index.html
-echo "</html>" >> index.html
+# tac index.html | awk 'NR > 2' | tac > tmp.html
+# cat tmp.html > index.html
+# rm tmp.html
+# cat counter.txt >> index.html
+# echo "" >> index.html
+# echo "</body>" >> index.html
+# echo "</html>" >> index.html
 
 git add .
 git commit -m "new update $(date)"
