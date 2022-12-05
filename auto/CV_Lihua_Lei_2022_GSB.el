b20c156a69f65f5ed043a9fbd8e0de05a85a1970
lihualei71 <lihualei@stanford.edu>
2022-12-04 20:09:54 -0800

    new update Sun Dec  4 20:09:53 PST 2022

--- auto/CV_Lihua_Lei_2022_GSB.el
+++ auto/CV_Lihua_Lei_2022_GSB.el
@@ -1,21 +1,24 @@
 (TeX-add-style-hook
- "CV_Lihua_Lei"
+ "CV_Lihua_Lei_2022_GSB"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("geometry" "left=3cm" "right=3cm")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
     "art10"
     "geometry"
-    "hyperref"))
+    "hyperref"
+    "xcolor")
+   (TeX-add-symbols
+    '("red" 1)))
  :latex)
 
